766f72f2bbcd4c173f2fe9620c6764033ee045ec
Moin Ahmed <moin.ahmed@ca.vu>
2025-01-27 00:35:48 +0000

    Initial solution creation

---
+++ HealthAssured.Checkout.Tests/CheckoutTests.cs
@@ -0,0 +1,15 @@
+namespace HealthAssured.Checkout.Tests;
+
+public class CheckoutTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ HealthAssured.Checkout/ICheckout.cs
@@ -0,0 +1,8 @@
+﻿namespace HealthAssured.Checkout;
+
+public interface ICheckout
+{
+    void Scan(string item);
+
+    int GetTotalPrice();
+}
